2c3a900cf36f0b5979110e6a9e633c88622f3975
Muskan Rajpoot <rajputmuskan060@gmail.com>
2025-08-11 16:49:58 +0530

    Add files via upload

---
+++ problem1094.java
@@ -0,0 +1,35 @@
+//----------CAR POOLING-------
+// There is a car with capacity empty seats. The vehicle only drives east (i.e., it cannot turn around and drive west).
+// You are given the integer capacity and an array trips where trips[i] = [numPassengersi, fromi, toi] indicates that the ith trip has numPassengersi passengers and the locations to pick them up and drop them off are fromi and toi respectively. The locations are given as the number of kilometers due east from the car's initial location.
+// Return true if it is possible to pick up and drop off all passengers for all the given trips, or false otherwise.
+
+// Example 1:
+
+// Input: trips = [[2,1,5],[3,3,7]], capacity = 4
+// Output: false
+// Example 2:
+
+// Input: trips = [[2,1,5],[3,3,7]], capacity = 5
+// Output: true
+
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int length[]=new int[1001];
+        for(int trip[]:trips){
+            int newpassenger=trip[0];
+            int from=trip[1];
+            int to=trip[2];
+            length[from]+=newpassenger; 
+            length[to]-=newpassenger;
+
+        }
+        int currentPassenger=0;
+        for(int i=0;i<=1000;i++){
+            currentPassenger+=length[i];
+           if(currentPassenger>capacity){
+                 return false;
+}
+        }
+return true;
+    }
+}
